bfdfb2ced288a688053605df6f9dba3a94687bba
Wade Kaiser <contact.kaiser.wade@gmail.com>
2020-11-08 13:43:41 -0500

    Final commit, players not working

--- src/commands/players.js
+++ src/commands/players.js
@@ -1,5 +1,25 @@
+const { getPlayers, getAllPlayers } = require('../queries/Players.js');
+
 module.exports = {
   name: 'players',
   description: 'Shows all TL current players and their social links!',
-  
+  usage: '[game] [playerName]',
+  async execute(message, args) {
+    const data = [];
+
+    if (!args.length) {
+      const players = await getPlayers('leagueoflegends');
+      data.push(players);
+
+      return message.author.send(data, { split: true })
+        .then(() => {
+          if (message.channel.type === 'dm') return;
+          message.reply('I\'ve sent you a DM with the full active player list!');
+        })
+        .catch((err) => {
+          console.error(`Could not send help DM to ${message.author.tag}.\n`, err);
+          message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+        });
+    }
+  },
 };
--- src/queries/Players.js
+++ src/queries/Players.js
@@ -1,28 +1,33 @@
+const FormData = require('form-data');
+const fetch = require('node-fetch');
 const supportedGames = ['leagueoflegends', 'counterstrike', 'dota2', 'rocketleague', 'smash', 'valorant'];
 
-const getPlayers = async (game) => {
+const getPlayers = (async (game) => {
   const formData = new FormData();
-  formData.append('wiki', game);
   formData.append('apikey', process.env.LIQUIDDB_API_KEY);
+  formData.append('wiki', game);
   formData.append('query', 'id,team,links');
   formData.append('conditions','[[team::Team Liquid]] AND [[links::!]]');
   
   const response = await fetch('https://api.liquipedia.net/api/v1/player', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Encoding': 'gzip'
     },
     body: formData,
-  }).catch(err => console.log('Error: ', err));
+  }).then(function(res) {
+    return res.json();
+  }).then(function(json) {
+    console.log(json);
+  }).catch(err => {
+    console.log('Error: ', err)
+  });
+});
 
-  return response;
-};
+const getAllPlayers = (async () => {
+  return await getPlayers(supportedGames[0]);
+});
 
-const getAllPlayers = async () => {
-  const players = [];
-  players = supportedGames.map(game => getPlayers(game));
-  return players;
-}
-
-module.getPlayers = getPlayers;
-module.getAllPlayers = getAllPlayers;
+exports.getPlayers = getPlayers;
+exports.getAllPlayers = getAllPlayers;
